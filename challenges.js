bae9323cdabf1edfc316e025d5f4b0416a097788
madtom85 <madtom75@gmail.com>
2017-12-04 11:15:17 -0800

    Pig Game Challenges

--- challenges.js
+++ challenges.js
@@ -53,15 +53,36 @@ btn();
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	if (gamePlaying) {
 		// 1. Random number
-	var dice = Math.floor(Math.random() * 6) + 1;
-	
+	var dice1 = Math.floor(Math.random() * 6) + 1;
+	var dice2 = Math.floor(Math.random() * 6) + 1;
 	// 2. Display The Result
-	var diceDOM = document.querySelector('.dice');
-	diceDOM.style.display = 'block';
-	diceDOM.src = 'dice-' + dice + '.png';
+	document.getElementById('dice-1').style.display = 'block';
+	document.getElementById('dice-2').style.display = 'block';
+	document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+	document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
 	// 3. Update the round score IF the rolled number is NOT 1
-	if (dice !== 1) {
+	if (dice1 !== 1 && dice2 !== 1) {
+		//Add Score
+		//roundScore = roundScore + dice;
+		roundScore += dice1 + dice2;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		//nextPlayer
+		nextPlayer();
+	}
+
+	}
+
+});	
+	/*
+	if (dice === 6 && lastDice === 6) {
+		//Player loses score
+		scores[activePlayer] = 0;
+		document.querySelector('#score-' + activePlayer).textContent = '0';
+		nextPlayer();
+	}
+	else if (dice !== 1) {
 		//Add Score
 		//roundScore = roundScore + dice;
 		roundScore += dice;
@@ -90,12 +111,14 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		document.querySelector('.dice').style.display = 'none';
 		*/
 		//nextPlayer
+		/*
 		nextPlayer();
 	}
-
+		lastDice = dice;
 	}
 
 });
+	*/
 
 	document.querySelector('.btn-hold').addEventListener('click', function() {
 		if (gamePlaying) {
@@ -104,10 +127,23 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		//Update UI
 		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+		var input = document.querySelector('.final-score').value;
+		var winningScore;
+		//undefined, null, 0 and "" are COERCED to false
+		// anything else is COERCED to true
+		if (input) {
+			winningScore = input;
+
+		} else{
+			winningScore = 100;
+		}
+
+
 		//Check which player won the game
-		if (scores[activePlayer] >= 100) {
+		if (scores[activePlayer] >= winningScore) {
 		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-		document.querySelector('.dice').style.display = 'none';
+		document.getElementById('dice-1').style.display = 'none';
+		document.getElementById('dice-2').style.display = 'none';
 		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
 		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 
@@ -133,7 +169,8 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		document.querySelector('.player-0-panel').classList.toggle('active');
 		document.querySelector('.player-1-panel').classList.toggle('active');
 
-		document.querySelector('.dice').style.display = 'none';
+		document.getElementById('dice-1').style.display = 'none';
+		document.getElementById('dice-2').style.display = 'none';
 	}
 	//not using an anonymous function since we don't want to call an anonymous function before another function
 	
@@ -145,6 +182,9 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 	activePlayer = 0;
 	gamePlaying = true;
 
+	document.getElementById('dice-1').style.display = 'none';
+	document.getElementById('dice-2').style.display = 'none';
+	
 	document.getElementById('score-0').textContent = '0';
 	document.getElementById('score-1').textContent = '0';
 	document.getElementById('current-0').textContent = '0';
